0c9cab8293766bf9776ef62739fafc4863540f87
AlexDev <minino-forever@rambler.ru>
2025-02-08 19:07:04 +0700

    add in SimpleExcepton class Car, Radio

---
+++ SimpleException/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    internal class Car
+    {
+        public const int MaXSpeed = 100;
+
+        public int CurrentSpeed { get; set; } = 0;
+
+        public string PetName { get; set; } = "";
+
+        private bool _carIsDead;
+
+        private readonly Radio _theMusicBox = new Radio();
+
+        public Car() { }
+
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            _theMusicBox.TurnOn(state);
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (_carIsDead)
+            {
+                Console.WriteLine($"{PetName} is out of order...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaXSpeed)
+                {
+                    Console.WriteLine($"{PetName} has overheated!");
+                    CurrentSpeed = 0;
+                    _carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine($"=> CurrentSpeed = {CurrentSpeed}");
+                }
+            }
+        }
+
+    }
+}
--- SimpleException/Program.cs
+++ SimpleException/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("******Simple Exception Example ******\n");
+
+            Console.WriteLine(" Creating a car and stepping on it!\n");
+
+            Car myCar = new Car("Zippe", 20);
+
+            myCar.CrankTunes(true);
+
+            for (int i = 0; i < 10; i++)
+            {
+                myCar.Accelerate(10);
+            }
         }
     }
 }
